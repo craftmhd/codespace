ab699bb83b91dff78bf6949956f95e15eaee5cfe
craftmhd <46298157+craftmhd@users.noreply.github.com>
2023-04-16 22:35:39 +0000

    Sun, Apr 16, 2023, 10:35 PM +00:00

---
+++ wall.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int r = 0; r < 3; r++)
+        printf("#");
+        printf("\n");
+    }
+    printf("#");
+    printf("\n");
+}
