b75102546e0582c44182bef2c0308734bbb8b98b
craftmhd <46298157+craftmhd@users.noreply.github.com>
2023-05-06 12:55:36 +0000

    Sat, May 6, 2023, 12:55 PM +00:00

--- hello.c
+++ hello.c
@@ -4,6 +4,5 @@
 int main()
 {
    string answer = get_string("what's your name? ");
-   string last = get_string("whats up bitch... :))))?");
-   printf("say, %s %s \n", answer, last);
+   printf("what's up %s bitch  \n", answer);
 }
